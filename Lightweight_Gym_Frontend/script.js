cf0aa65cc6e4fac06097a3cb8a9b75624ccd3db5
brianabeltimothy <brianabeltimothyd@gmail.com>
2023-05-31 10:47:43 +0200

    add new file memberships-login modify the rest for communication

--- Lightweight_Gym_Frontend/script.js
+++ Lightweight_Gym_Frontend/script.js
@@ -1,19 +1,21 @@
-// Get the form element
 const loginForm = document.getElementById('loginForm');
+const loginButton = document.getElementById('loginButton');
 
-// Add an event listener to the form submit event
-loginForm.addEventListener('submit', async (e) => {
-  e.preventDefault(); // Prevent form submission
+const signUpForm = document.getElementById('signupForm');
+const signUpButton = document.getElementById('signupButton');
 
-  // Get the input values
+loginForm?.addEventListener('submit', async (e) => {
+  e.preventDefault(); 
+
+
   const email = document.getElementById('emailInput').value;
   const password = document.getElementById('passwordInput').value;
 
-  // Create an object with the input data
+
   const signInData = { email, password };
 
   try {
-    // Make the HTTP POST request to the API endpoint
+
     const response = await fetch('http://localhost:5296/api/Accounts/signin', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -21,18 +23,58 @@ loginForm.addEventListener('submit', async (e) => {
     });
 
     if (response.ok) {
-      // Handle successful response
       const result = await response.json();
       console.log('User signed in successfully:', result);
-      // Redirect
       window.location.href = 'index-login.html';
     } else {
-      // Handle error response
       console.error('Sign-in failed');
-      // Perform any desired error handling, such as displaying an error message to the user
     }
   } catch (error) {
     console.error('An error occurred during sign-in:', error);
-    // Perform any desired error handling, such as displaying an error message to the user
   }
+});
+
+loginButton?.addEventListener('click', () => {
+  loginForm.submit();
+});
+
+
+signUpForm?.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const firstName = document.getElementById('firstNameInput').value;
+  const lastName = document.getElementById('lastNameInput').value;
+  const email = document.getElementById('emailInput').value;
+  const password = document.getElementById('passwordInput').value;
+  const confirmPassword = document.getElementById('confirmPasswordInput').value;
+
+  const signUpData = {
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmPassword
+  };
+
+  try {
+    const response = await fetch('http://localhost:5296/api/Accounts/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(signUpData),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      console.log('User signed up successfully:', result);
+      window.location.href = 'login.html';
+    } else {
+      console.error('Sign-up failed');
+    }
+  } catch (error) {
+    console.error('An error occurred during sign-up:', error);
+  }
+});
+
+signUpButton?.addEventListener('click', () => {
+  signUpForm.submit();
 });
